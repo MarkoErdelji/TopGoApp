3950c2a61ca56d9f09feee2a6ce9df2719d51bf9
MarkoErdelji <erdeljimarko@gmail.com>
2023-01-16 01:35:40 +0100

    feat: Added form for driver profile edit aswell as a dialog for password changing

--- app/src/main/java/com/example/uberapp_tim6/driver/fragments/DriverEditProfileFragment.java
+++ app/src/main/java/com/example/uberapp_tim6/driver/fragments/DriverEditProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.uberapp_tim6.driver.fragments;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,9 +10,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.uberapp_tim6.DTOS.UserInfoDTO;
 import com.example.uberapp_tim6.R;
+import com.example.uberapp_tim6.driver.DriverMainActivity;
+import com.example.uberapp_tim6.tools.FragmentTransition;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -41,8 +48,6 @@ public class DriverEditProfileFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment EditProfileFragment.
      */
     // TODO: Rename and change types and number of parameters
@@ -70,5 +75,49 @@ public class DriverEditProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        Button back = view.findViewById(R.id.edit_driver_back);
+        DriverMainActivity dvm = (DriverMainActivity) this.getActivity();
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransition.to(DriverProfileFragment.newInstance(driver),dvm,false,R.id.mainContent);
+            }
+        });
+        ImageView img = view.findViewById(R.id.profileIcon);
+        Glide.with(getContext()).load(driver.getProfilePicture()).into(img);
+        TextView firstName = view.findViewById(R.id.nameValue);
+        TextView lastName = view.findViewById(R.id.surnameValue);
+        TextView Username = view.findViewById(R.id.usernameValue);
+        TextView PhoneNumber = view.findViewById(R.id.phoneNumberValue);
+        TextView Address = view.findViewById(R.id.addressValue);
+
+        firstName.setText(driver.getName());
+        lastName.setText(driver.getSurname());
+        Username.setText(driver.getEmail());
+        PhoneNumber.setText(driver.getTelephoneNumber());
+        Address.setText(driver.getAddress());
+
+        TextView changePasswordText = view.findViewById(R.id.changePasswordText);
+        LayoutInflater inflater = getLayoutInflater();
+        View changePasswordDialogView = inflater.inflate(R.layout.change_password_dialog, null);
+        AlertDialog.Builder builder2 = new AlertDialog.Builder(this.getContext());
+        builder2.setView(changePasswordDialogView);
+        AlertDialog Dialog = builder2.create();
+        Dialog.setCancelable(false);
+        Button cancelButton = changePasswordDialogView.findViewById(R.id.btn_cancel);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog.dismiss();
+            }
+        });
+
+        changePasswordText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog.show();
+            }
+        });
     }
 }
